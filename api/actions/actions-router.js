bbcc7d029edd5344bf15c132a20c8ed9f0ec5aa8
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-14 02:06:48 -0500

    get endpoint for all actions tested and working

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,7 +1,18 @@
 // Write your "actions" router here!
+const express = require("express");
+const router = express.Router();
+const Action = require("./actions-model");
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res, next) => {
+  Action.get()
+    .then((action) => {
+      res.json(action);
+    })
+    .catch(next);
+});
 router.get("/", (req, res) => {});
 router.post("/", (req, res) => {});
 router.put("/", (req, res) => {});
 router.delete("/", (req, res) => {});
+
+module.exports = router;
