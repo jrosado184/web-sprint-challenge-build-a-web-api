c2ee422d9024ee9cb09b15ed08b9f420cfa45fea
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-13 19:43:52 -0500

    get router enpoints for projects built, tested and working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,28 @@
 // Write your "projects" router here!
+const Projects = require("./projects-model");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const projects = await Projects.get();
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const projects = await Projects.get(req.params.id);
+    if (!projects) {
+      res.status(404).json({ message: "no project with that id exists" });
+    } else {
+      res.json(projects);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
